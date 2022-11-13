622e9c38c08b698c4cb503bc7b7ad4de0c3e19ca
France Emiele Ariola <franceemiele.ariola@tup.edu.ph>
2022-11-14 02:32:07 +0800

    final lab2.2

---
+++ LAB2-2.ino
@@ -0,0 +1,290 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+#define BLYNK_AUTH_TOKEN "NkjVq23XELM5oV3sSJhPZ2XzgFWzuLE8"
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "adamseif";//wifi name
+char pass[] = "franasen23";//wifi password
+
+const int A = 12;
+const int B = 14;
+const int C = 2;
+const int D = 5;
+const int E = 4;
+const int F = 13;
+const int G = 15;
+const int DP = 0;
+int del = 500;
+
+void zero(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+
+}
+
+void one(){
+
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+
+}
+
+void two(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void three(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void four(){
+
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void five(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void six(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void seven(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+
+}
+
+void eight(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void nine(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void a(){
+  
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void b(){
+
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void c(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(DP, LOW);
+
+}
+
+void d(){
+
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void e(){
+  
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+void f(){
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(DP, LOW);
+
+}
+
+
+BLYNK_WRITE(V0) {
+ 
+  if(param.asInt() == 1){
+      f();
+      delay(del);
+      e();
+      delay(del);
+      d();
+      delay(del);
+      c();
+      delay(del);
+      b();
+      delay(del);
+      a();
+      delay(del);
+      nine();
+      delay(del);
+      eight();
+      delay(del);   
+      seven();
+      delay(del);  
+      six();
+      delay(del);
+      five();
+      delay(del);
+      four();
+      delay(del);
+      three();
+      delay(del);
+      two();
+      delay(del);
+      one();
+      delay(del);
+      zero();
+      delay(del);
+      Blynk.syncVirtual(V0);
+      }
+      else
+      zero();
+      
+}
+
+void setup() {
+  
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(DP, OUTPUT);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  
+}
+
+void loop() {
+  
+  Blynk.run();
+  
+}
